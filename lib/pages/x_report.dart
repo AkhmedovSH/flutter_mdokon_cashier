5097ef16a3ff0a6ed33b7364f008285c66454ce9
Shokhrukh <aiiiypuk@mail.ru>
2022-02-08 15:26:57 +0500

    x-report fix

--- lib/pages/x_report.dart
+++ lib/pages/x_report.dart
@@ -65,135 +65,170 @@ class _XReportState extends State<XReport> {
     );
   }
 
+  buildRow2(text, text2, {fz = 16.0}) {
+    return Column(
+      children: [
+        Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Expanded(
+                flex: 6,
+                child: Text(
+                  text,
+                  style: TextStyle(
+                      fontWeight: FontWeight.w600, color: b8, fontSize: fz),
+                )),
+            Expanded(
+              flex: 3,
+              child: Text(
+                '${formatMoney(text2)}',
+                textAlign: TextAlign.end,
+                style: TextStyle(color: b8, fontSize: fz),
+              ),
+            )
+          ],
+        ),
+        Divider(color: Colors.black)
+      ],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      key: _scaffoldKey,
-      appBar: AppBar(
-        systemOverlayStyle: SystemUiOverlayStyle(
-          statusBarIconBrightness: Brightness.dark,
-          statusBarColor: white, // Status bar
-        ),
-        bottomOpacity: 0.0,
-        backgroundColor: white,
-        elevation: 0,
-        // centerTitle: true,
-        leading: IconButton(
-          onPressed: () {
-            _scaffoldKey.currentState!.openDrawer();
-          },
-          icon: Icon(Icons.menu, color: black,),
-        ),
-      ),
-      drawer: SizedBox(
-        width: MediaQuery.of(context).size.width * 0.70,
-        child: const DrawerAppBar(),
-      ),
-        body: SafeArea(
-      child: SingleChildScrollView(
-        child: Container(
-          padding: EdgeInsets.symmetric(horizontal: 12),
-          child: Column(
-            children: [
-              Center(
-                  child: Image.asset(
-                'images/logo.jpg',
-                height: 64,
-                width: 200,
-              )),
-              Container(
-                alignment: Alignment.center,
-                margin: EdgeInsets.only(bottom: 10),
-                child: Text(
-                  'ДУБЛИКАТ',
-                  style: TextStyle(
-                      fontWeight: FontWeight.w700, color: b8, fontSize: 18),
-                ),
-              ),
-              Container(
-                alignment: Alignment.center,
-                margin: EdgeInsets.only(bottom: 10),
-                child: Text(
-                  '${report['posName']}',
-                  style: TextStyle(
-                      fontWeight: FontWeight.w700, color: b8, fontSize: 16),
-                ),
-              ),
-              Container(
-                alignment: Alignment.center,
-                margin: EdgeInsets.only(bottom: 10),
-                child: Text(
-                  'Телефон: 998977655885',
-                  style: TextStyle(
-                      fontWeight: FontWeight.w700, color: b8, fontSize: 16),
-                ),
-              ),
-              Container(
-                alignment: Alignment.center,
-                margin: EdgeInsets.only(bottom: 10),
-                child: Text(
-                  'Адресс: Glinka',
-                  style: TextStyle(
-                      fontWeight: FontWeight.w700, color: b8, fontSize: 16),
-                ),
-              ),
-              buildRow('Кассир', report['cashierName']),
-              buildRow('Касса №', report['shiftNumber']),
-              buildRow('ИНН', report['tin'] ?? ''),
-              buildRow('Дата', report['shiftOpenDate']),
-              Container(
-                margin: EdgeInsets.symmetric(vertical: 5),
-                child: Text(
-                  '*****************************************************************************************',
-                  style: TextStyle(color: b8),
-                  overflow: TextOverflow.clip,
-                  maxLines: 1,
-                  softWrap: false,
-                ),
-              ),
-              for (var i = 0; i < reportList.length; i++)
-              reportList[i]['amountIn'] != 0 ? 
-              buildRow('${reportList[i]['paymentTypeName']} ${reportList[i]['paymentPurposeName']} Приход (${reportList[i]['currencyName']}) ', reportList[i]['amountIn'])
-              :
-              buildRow('${reportList[i]['paymentTypeName']} ${reportList[i]['paymentPurposeName']} Расход (${reportList[i]['currencyName']}) ', reportList[i]['amountOut']),
-              Container(
-                margin: EdgeInsets.symmetric(vertical: 5),
-                child: Text(
-                  '*****************************************************************************************',
-                  style: TextStyle(color: b8),
-                  overflow: TextOverflow.clip,
-                  maxLines: 1,
-                  softWrap: false,
-                ),
-              ),
-              buildRow('Продано в долг', report['debt']),
-              buildRow('Сумма скидки', report['discountAmount']),
-              Container(
-                margin: EdgeInsets.symmetric(vertical: 5),
-                child: Text(
-                  '*****************************************************************************************',
-                  style: TextStyle(color: b8),
-                  overflow: TextOverflow.clip,
-                  maxLines: 1,
-                  softWrap: false,
-                ),
-              ),
-              buildRow('Остаток в кассе', report['cashboxTotalAmount']),
-              Container(
-                margin: EdgeInsets.symmetric(vertical: 5),
-                child: Text(
-                  '*****************************************************************************************',
-                  style: TextStyle(color: b8),
-                  overflow: TextOverflow.clip,
-                  maxLines: 1,
-                  softWrap: false,
-                ),
-              ),
-              buildRow('КОЛИЧЕСТВО Х ОТЧЕТОВ', report['countRequest']),
-            ],
+        key: _scaffoldKey,
+        appBar: AppBar(
+          systemOverlayStyle: SystemUiOverlayStyle(
+            statusBarIconBrightness: Brightness.dark,
+            statusBarColor: white, // Status bar
+          ),
+          bottomOpacity: 0.0,
+          backgroundColor: white,
+          elevation: 0,
+          // centerTitle: true,
+          leading: IconButton(
+            onPressed: () {
+              _scaffoldKey.currentState!.openDrawer();
+            },
+            icon: Icon(
+              Icons.menu,
+              color: black,
+            ),
           ),
         ),
-      ),
-    ));
+        drawer: SizedBox(
+          width: MediaQuery.of(context).size.width * 0.70,
+          child: const DrawerAppBar(),
+        ),
+        body: SafeArea(
+          child: SingleChildScrollView(
+            child: Container(
+              padding: EdgeInsets.symmetric(horizontal: 12),
+              child: Column(
+                children: [
+                  Center(
+                      child: Image.asset(
+                    'images/logo.jpg',
+                    height: 64,
+                    width: 200,
+                  )),
+                  Container(
+                    alignment: Alignment.center,
+                    margin: EdgeInsets.only(bottom: 10),
+                    child: Text(
+                      'ДУБЛИКАТ',
+                      style: TextStyle(
+                          fontWeight: FontWeight.w700, color: b8, fontSize: 18),
+                    ),
+                  ),
+                  Container(
+                    alignment: Alignment.center,
+                    margin: EdgeInsets.only(bottom: 10),
+                    child: Text(
+                      '${report['posName']}',
+                      style: TextStyle(
+                          fontWeight: FontWeight.w700, color: b8, fontSize: 16),
+                    ),
+                  ),
+                  Container(
+                    alignment: Alignment.center,
+                    margin: EdgeInsets.only(bottom: 10),
+                    child: Text(
+                      'Телефон: 998977655885',
+                      style: TextStyle(
+                          fontWeight: FontWeight.w700, color: b8, fontSize: 16),
+                    ),
+                  ),
+                  Container(
+                    alignment: Alignment.center,
+                    margin: EdgeInsets.only(bottom: 10),
+                    child: Text(
+                      'Адресс: Glinka',
+                      style: TextStyle(
+                          fontWeight: FontWeight.w700, color: b8, fontSize: 16),
+                    ),
+                  ),
+                  buildRow('Кассир', report['cashierName']),
+                  buildRow('Касса №', report['shiftNumber']),
+                  buildRow('ИНН', report['tin'] ?? ''),
+                  buildRow('Дата', report['shiftOpenDate']),
+                  Container(
+                    margin: EdgeInsets.symmetric(vertical: 5),
+                    child: Text(
+                      '*****************************************************************************************',
+                      style: TextStyle(color: b8),
+                      overflow: TextOverflow.clip,
+                      maxLines: 1,
+                      softWrap: false,
+                    ),
+                  ),
+                  for (var i = 0; i < reportList.length; i++)
+                    reportList[i]['amountIn'] != 0
+                        ? buildRow2(
+                            '${reportList[i]['paymentTypeName']} ${reportList[i]['paymentPurposeName']} Приход (${reportList[i]['currencyName']}) ',
+                            reportList[i]['amountIn'])
+                        : buildRow2(
+                            '${reportList[i]['paymentTypeName']} ${reportList[i]['paymentPurposeName']} Расход (${reportList[i]['currencyName']}) ',
+                            reportList[i]['amountOut']),
+                  Container(
+                    margin: EdgeInsets.symmetric(vertical: 5),
+                    child: Text(
+                      '*****************************************************************************************',
+                      style: TextStyle(color: b8),
+                      overflow: TextOverflow.clip,
+                      maxLines: 1,
+                      softWrap: false,
+                    ),
+                  ),
+                  buildRow('Продано в долг', report['debt']),
+                  buildRow('Сумма скидки', report['discountAmount']),
+                  Container(
+                    margin: EdgeInsets.symmetric(vertical: 5),
+                    child: Text(
+                      '*****************************************************************************************',
+                      style: TextStyle(color: b8),
+                      overflow: TextOverflow.clip,
+                      maxLines: 1,
+                      softWrap: false,
+                    ),
+                  ),
+                  buildRow('Остаток в кассе', report['cashboxTotalAmount']),
+                  Container(
+                    margin: EdgeInsets.symmetric(vertical: 5),
+                    child: Text(
+                      '*****************************************************************************************',
+                      style: TextStyle(color: b8),
+                      overflow: TextOverflow.clip,
+                      maxLines: 1,
+                      softWrap: false,
+                    ),
+                  ),
+                  buildRow('КОЛИЧЕСТВО Х ОТЧЕТОВ', report['countRequest']),
+                  SizedBox(height: 20)
+                ],
+              ),
+            ),
+          ),
+        ));
   }
 }
